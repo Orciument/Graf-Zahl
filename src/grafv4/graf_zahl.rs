c35292c74b7d616a2a61268f6901b7f2f098c68d
orciument <orciument@gmail.com>
2024-04-05 16:35:43 +0200

    Remove CountResult and replace it with Tuple

--- src/grafv4/graf_zahl.rs
+++ src/grafv4/graf_zahl.rs
@@ -5,12 +5,11 @@ use crate::grafv4::countable::Countable;
 use crate::grafv4::io_reader::{read_dir, read_file};
 use crate::grafzahl::ignore_checker;
 
-#[derive(Default)]
-struct CountResult<CountMode: Countable> {
-    value_string: String,
-    value: CountMode,
-    member_strings: Vec<String>,
-    ignored: bool
+#[derive(Default, Clone)]
+struct TreeNode {
+    string: String,
+    members: Vec<TreeNode>,
+    ignored: bool,
 }
 
 pub fn count_entrypoint(path: PathBuf, state: &AppState) {
@@ -18,12 +17,12 @@ pub fn count_entrypoint(path: PathBuf, state: &AppState) {
 
     //TODO match for all CountModes, set the type parameter, and give a lambda with the right classifier
 
-    // let count = count_path::<countModeType>(path, state);
+    // let count = count_path::<countModeType>(path, state).0;
 
     //TODO display
 }
 
-fn count_path<CountMode: Countable>(path: PathBuf, state: &AppState) -> CountResult<CountMode> {
+fn count_path<CountMode: Countable>(path: PathBuf, state: &AppState) -> (TreeNode, CountMode) {
     assert!(path.is_absolute(), "Received Filepath is not absolut! {}", &path.display());
     assert!(path.exists(), "No File/Folder exists at this Path: {}", &path.display());
 
@@ -32,10 +31,11 @@ fn count_path<CountMode: Countable>(path: PathBuf, state: &AppState) -> CountRes
         .to_str().expect("Unable to convert File/Folder Name into Unicode!").to_string();
 
     if ignore_checker::check_if_ignored(&path, &state) {
-        return CountResult {
-            value_string: format!("{name} is ignored!"),
-            .. Default::default()
-        };
+        return (TreeNode {
+            string: format!("{name} is ignored!"),
+            ignored: true,
+            ..Default::default()
+        }, CountMode::default());
     }
 
     return if path.is_dir() {
@@ -46,7 +46,7 @@ fn count_path<CountMode: Countable>(path: PathBuf, state: &AppState) -> CountRes
     };
 }
 
-fn count_file<CountMode: Countable>(path: PathBuf, name: String) -> CountResult<CountMode> {
+fn count_file<CountMode: Countable>(path: PathBuf, name: String) -> (TreeNode, CountMode) {
     let ext = path.extension()
         .unwrap_or_else(|| OsStr::new(""))
         .to_str().expect("Can't convert Filename into UTF-8 String!");
@@ -54,26 +54,25 @@ fn count_file<CountMode: Countable>(path: PathBuf, name: String) -> CountResult<
     let file = match read_file(&path) {
         Ok(v) => v,
         Err(e) => {
-            return CountResult {
-                value_string: format!("{name} -> Err: {e}"),
-                .. Default::default()
-            };
+            return (TreeNode {
+                string: format!("{name} -> Err: {e}"),
+                ..Default::default()
+            }, CountMode::default());
         }
     };
-    return CountResult {
-        value_string: format!("{name} ->"),
-        value: *CountMode::count(file, ext),
-        .. Default::default()
-    };
+    return (TreeNode {
+        string: format!("{name} ->"),
+        ..Default::default()
+    }, *CountMode::count(file, ext));
 }
 
-fn count_folder<CountMode: Countable>(path: &PathBuf, state: &AppState, name: &String) -> CountResult<CountMode> {
-    let members: Vec<CountResult<CountMode>> = match read_dir(path) {
+fn count_folder<CountMode: Countable>(path: &PathBuf, state: &AppState, name: &String) -> (TreeNode, CountMode) {
+    let members: Vec<(TreeNode, CountMode)> = match read_dir(path) {
         Err(e) => {
-            return CountResult {
-                value_string: format!("{name} -> Err: {e}"),
-                .. Default::default()
-            };
+            return (TreeNode {
+                string: format!("{name} -> Err: {e}"),
+                ..Default::default()
+            }, CountMode::default());
         }
         Ok(v) => {
             v.into_iter()
@@ -81,12 +80,11 @@ fn count_folder<CountMode: Countable>(path: &PathBuf, state: &AppState, name: &S
                 .collect()
         }
     };
-    let member_sum = members.iter().map(|x| x.value.clone()).sum();
-    let member_strings = members.iter().map( |x| x.value_string.clone()).collect();
-    return CountResult {
-        value_string: format!("{name} -> {member_sum}"),
-        value: member_sum,
-        member_strings,
+    let member_sum = members.iter().map(|x| x.1.clone()).sum();
+    let member_strings = members.iter().map(|x| x.0.clone()).collect();
+    return (TreeNode {
+        string: format!("{name} -> {member_sum}"),
+        members: member_strings,
         ignored: false,
-    };
+    }, member_sum);
 }
