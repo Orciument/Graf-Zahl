419245b048f1c773d9aa1a33adc6a2cf55eae6b4
orciument <orciument@gmail.com>
2024-04-06 15:25:14 +0200

    Added state and result to count, added Lang name to results

--- src/grafv4/count_modes/line.rs
+++ src/grafv4/count_modes/line.rs
@@ -1,10 +1,12 @@
+use std::fmt::Display;
+use crate::AppState;
 use crate::grafv4::countable::Countable;
 
 pub(crate) type LineCount = u32;
 
 impl Countable for LineCount {
-    fn count(content: Vec<String>, _: &str) -> Box<Self> {
-        return Box::from(content.len() as LineCount);
+    fn count(content: Vec<String>, _: &str, _: &AppState) -> Result<Self, String> {
+        return Ok(content.len() as LineCount);
     }
 
     fn display_summary(self, project_name: String) {
--- src/grafv4/countable.rs
+++ src/grafv4/countable.rs
@@ -1,9 +1,10 @@
 use std::fmt::Display;
 use std::iter::Sum;
 use std::ops::Add;
+use crate::AppState;
 
 pub(crate) trait Countable: Add + Sum + Display + Default + Clone {
-    fn count(content: Vec<String>, extension: &str) -> Box<Self>;
+    fn count(content: Vec<String>, extension: &str, state: &AppState) -> Result<Self, String>;
 
     fn display_summary(self, project_name: String);
 
