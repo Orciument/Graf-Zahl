30a55c6fa12e18507cdbcdcf7a706ed242ddf712
orciument <orciument@gmail.com>
2024-04-12 19:29:48 +0200

    rename some structs, and simplify a some things a bit

--- src/grafzahl/language/tokeniser/rust_test.rs
+++ src/grafzahl/language/tokeniser/rust_test.rs
@@ -8,8 +8,6 @@ impl Annotator for Rust {
         let mut vec: Vec<Annotation> = vec![];
         let mut multi_comment_depth: i32 = 0;
         for line in content {
-            let mut remaining: &str = line.as_str();
-
             // early return if line has line comment
             let single_comment = next_symbol_pos(&line, vec!["//", "///"]);
             if single_comment.is_some() {
@@ -24,82 +22,70 @@ impl Annotator for Rust {
             }
             // if line has multiline comment
             let multi_line_symbols = vec![
-                Symbol { kind: SymbolKind::START, symbol: "/*".to_string() },
-                Symbol { kind: SymbolKind::END, symbol: "*/".to_string() },
+                BlockSymbols { start: true, symbol: "/*".to_string() },
+                BlockSymbols { start: false, symbol: "*/".to_string() },
             ];
-            let mut prefix: &str = "";
-            loop {
-                match get_next_symbol(&remaining, &multi_line_symbols) {
-                    Token::MultiStart(index, len) => {
-                        let sub = remaining.split_at(index);
-                        if multi_comment_depth <= 0 {
-                            vec.push(Annotation::Code(String::from(prefix.to_owned() + sub.0)));
-                        } else {
-                            vec.push(Annotation::Comment(String::from(prefix.to_owned() + sub.0)));
-                        }
-                        prefix = &sub.1[..len];
-                        remaining = &sub.1[len..];
-                        multi_comment_depth += 1;
-                    }
-                    Token::MultiEnd(index, len) => {
-                        let sub = remaining.split_at(index + len);
-                        if multi_comment_depth <= 0 {
-                            vec.push(Annotation::Code(String::from(prefix.to_owned() + sub.0)));
-                        } else {
-                            vec.push(Annotation::Comment(String::from(prefix.to_owned() + sub.0)));
-                        }
-                        prefix = "";
-                        remaining = sub.1;
-                        multi_comment_depth -= 1;
-                    }
-                    Token::LineEnd => {
-                        if multi_comment_depth <= 0 {
-                            vec.push(Annotation::Code(String::from(prefix.to_owned() + remaining)));
-                        } else {
-                            vec.push(Annotation::Comment(String::from(prefix.to_owned() + remaining)));
-                        }
-                        break;
-                    }
-                }
-            }
+            parse_multi_comments(&mut vec, multi_comment_depth, line.as_str(), &multi_line_symbols);
         }
         vec
     }
 }
 
-struct Symbol {
-    kind: SymbolKind,
-    symbol: String,
-}
-
-#[derive(Clone)]
-enum SymbolKind {
-    START,
-    END,
-}
-
-
-struct TokenPos {
-    kind: SymbolKind,
-    symbol: String,
-    index: usize,
-}
-
-impl Eq for TokenPos {}
-
-impl PartialEq<Self> for TokenPos {
-    fn eq(&self, other: &Self) -> bool {
-        self.index.eq(&other.index)
+/// consumes all Block Comment Symbols until the next linebreak, ignores line comments
+fn parse_multi_comments(vec: &mut Vec<Annotation>, mut multi_comment_depth: i32, line: &str, multi_line_symbols: &Vec<BlockSymbols>) {
+    let mut remaining: &str = line;
+    let mut prefix: &str = "";
+    loop {
+        match get_next_symbol(&remaining, &multi_line_symbols) {
+            Token::MultiStart(index, len) => {
+                let sub = remaining.split_at(index);
+                push_string(vec, multi_comment_depth, prefix, sub.0);
+                prefix = &sub.1[..len];
+                remaining = &sub.1[len..];
+                multi_comment_depth += 1;
+            }
+            Token::MultiEnd(index, len) => {
+                let sub = remaining.split_at(index + len);
+                push_string(vec, multi_comment_depth, prefix, sub.0);
+                prefix = "";
+                remaining = sub.1;
+                multi_comment_depth -= 1;
+            }
+            Token::LineEnd => {
+                push_string(vec, multi_comment_depth, prefix, remaining);
+                break;
+            }
+        }
     }
 }
 
-impl PartialOrd<Self> for TokenPos {
+fn push_string(mut vec: &mut Vec<Annotation>, multi_comment_depth: i32, prefix: &str, remaining: &str) {
+    if multi_comment_depth <= 0 {
+        vec.push(Annotation::Code(String::from(prefix.to_owned() + remaining)));
+    } else {
+        vec.push(Annotation::Comment(String::from(prefix.to_owned() + remaining)));
+    }
+}
+/// Multiline comment Symbol
+struct BlockSymbols {
+    start: bool,
+    symbol: String,
+}
+
+#[derive(Eq, PartialEq)]
+struct BlockSymbolPos {
+    index: usize,
+    start: bool,
+    symbol: String,
+}
+
+impl PartialOrd<Self> for BlockSymbolPos {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.index.partial_cmp(&other.index)
     }
 }
 
-impl Ord for TokenPos {
+impl Ord for BlockSymbolPos {
     fn cmp(&self, other: &Self) -> Ordering {
         self.index.cmp(&other.index)
     }
@@ -113,6 +99,7 @@ fn next_symbol_pos(line: &String, symbols: Vec<&str>) -> Option<usize> {
         .min()
 }
 
+#[derive(Eq, PartialEq)]
 enum Token {
     MultiStart(usize, usize),
     MultiEnd(usize, usize),
@@ -120,7 +107,7 @@ enum Token {
 }
 
 /// None, means that next important character is lineEnd
-fn get_next_symbol(line: &str, symbols: &Vec<Symbol>) -> Token {
+fn get_next_symbol(line: &str, symbols: &Vec<BlockSymbols>) -> Token {
     let t_pos = symbols.into_iter()
         .map(|x| find_symbol(line, x))
         .filter(|x1| x1.is_some())
@@ -130,16 +117,16 @@ fn get_next_symbol(line: &str, symbols: &Vec<Symbol>) -> Token {
         return Token::LineEnd;
     }
     let pos = t_pos.unwrap();
-    return match pos.kind {
-        SymbolKind::START => Token::MultiStart(pos.index, pos.symbol.len()),
-        SymbolKind::END => Token::MultiEnd(pos.index, pos.symbol.len())
+    return match pos.start {
+        true => Token::MultiStart(pos.index, pos.symbol.len()),
+        false => Token::MultiEnd(pos.index, pos.symbol.len())
     };
 }
 
-fn find_symbol(line: &str, symbol: &Symbol) -> Option<TokenPos> {
-    Some(TokenPos {
-        kind: symbol.kind.clone(),
-        symbol: symbol.symbol.to_string(),
+fn find_symbol(line: &str, symbol: &BlockSymbols) -> Option<BlockSymbolPos> {
+    Some(BlockSymbolPos {
         index: line.find(symbol.symbol.as_str())?,
+        start: symbol.start,
+        symbol: symbol.symbol.to_string(),
     })
 }
