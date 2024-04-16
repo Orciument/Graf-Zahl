70ee7076de83e3d4a175580b275f7bf03f7dc89c
orciument <orciument@gmail.com>
2024-04-16 16:21:34 +0200

    add comment support for langfile, support empty lang symbol array, change tree display Symbols

--- src/grafzahl/language/lang_parser.rs
+++ src/grafzahl/language/lang_parser.rs
@@ -1,11 +1,19 @@
 use colored::Colorize;
 use regex::Regex;
+use structopt::lazy_static::lazy_static;
 use crate::grafzahl::language::languages::Language;
+
+lazy_static! {
+    static ref REGEX: Regex = Regex::new("^ *\"(.*)\" *, *\"(.*)?\" *, *\\[(.*)] *, *\\[(.*)] *, *\\[(.*)] *$").unwrap();
+}
+
 
 pub fn parse_langs(content: Vec<String>) -> Vec<Language> {
     let mut vec = vec![];
     for line in content {
         if line.trim().is_empty() {
+            continue;
+        } else if line.trim().starts_with("#") {
             continue;
         }
         match parse_line(&line) {
@@ -14,7 +22,7 @@ pub fn parse_langs(content: Vec<String>) -> Vec<Language> {
                 eprintln!("{}", "Couldn't parse line Language Definition is malformed!".red().underline());
                 eprintln!("{}", line.red());
                 eprintln!(" ");
-            },
+            }
         }
     }
     vec
@@ -22,9 +30,8 @@ pub fn parse_langs(content: Vec<String>) -> Vec<Language> {
 
 /// Parse a language File line using Regex capturing groups, whitespace is not significant. A Unit Error is returned when the Regex did not match the given input line
 fn parse_line(line: &String) -> Result<Language, ()> {
-    let r = Regex::new("^ *\"(.*)\" *, *\"(.*)?\" *, *\\[(.*)] *, *\\[(.*)] *, *\\[(.*)] *$").unwrap();
     let test_line = r#" "rs", "Rust", [ "//", "///"], ["/*"], ["*/"]"#;
-    let caps = r.captures(line).ok_or(())?;
+    let caps = REGEX.captures(line).ok_or(())?;
     // these shouldn't be able to fail because all our match groups are mandatory
     // and the capturing itself should fail if they aren't provided by the user
     let extension = caps.get(1).unwrap().as_str();
@@ -46,6 +53,12 @@ fn parse_line(line: &String) -> Result<Language, ()> {
 fn parse_array(raw: &str) -> Vec<String> {
     raw.split(",")
         .map(|x| x.trim())
+        .filter(|x2| {
+            if x2.len() == 1 {
+                eprintln!("{}: A Symbol Definition has a unclosed String!\n", "warning".bright_yellow().bold());
+            }
+            x2.len() >= 2
+        })
         .map(|x1| x1[1..x1.len() - 1].replace("\\\"", "\""))
         .collect()
 }
